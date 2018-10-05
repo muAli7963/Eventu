05b9ca929fa321f26444510953a49dbbb60fa8bb
diego7963 <elshrab90.ma@gmail.com>
2018-10-05 15:43:01 +0200

    finish highlights and info

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './resources/styles.css'
 import Header from './components/header_footer/Header'
 import Featured from './components/featured';
+import EventNFO from './components/eventu_info'
+import Highlights from './components/Highlights'
+
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+      <div className="App" style={{ height:"1500px"}}>
         <Header />
         <Featured />
+        <EventNFO />
+        <Highlights />
       </div>
     );
   }
